bbefc94fb7c6adba63d51e2a0d3e296e12ff4dcf
YannMOUSSAN <46843092+YannMOUSSAN@users.noreply.github.com>
2019-12-26 03:31:02 +0100

    control palyer + gards ia in progress

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+    bool isFacingRight = true;
+    public Transform startSight, endSight;
+    bool seesPlayer = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.DrawLine(startSight.position, endSight.position, Color.green);
+        while (seesPlayer == false)
+        {
+            Debug.DrawLine(startSight.position, endSight.position, Color.green);
+        }
+
+        while (seesPlayer == true)
+        {
+            Debug.DrawLine(startSight.position, endSight.position, Color.magenta);
+        }
+    }
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-3, 0);
+            animator.Play("player walk");
+            spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(3, 0);
+            animator.Play("player walk");
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+            animator.Play("player idle");
+        }
+    }
+}
